6488db89c2ddc002fbee144d5d90e01c509f0980
Michał Gold <bond.gold@wp.pl>
2025-02-11 00:59:20 +0100

    Added link to CodeWars

--- src/components/Sections/Section_OMnie.vue
+++ src/components/Sections/Section_OMnie.vue
@@ -11,7 +11,9 @@ const [lang, strings] = use_lang({
             <span class="soft"> (completely unrelated languages, but that's how it turned out)</span>. <br>
             My first contact with programming was in middle school, and I've been writing code ever since. <br>
             I treat programming as a very complex logical puzzle <span class="soft"> (and <b>I love logical puzzles</b>)</span>. <br>
-            From time to time I solve tasks related to <b>algorithmics</b>, on sites such as <b>CodeWars</b> or <b>LeetCode</b>. <br>
+            From time to time I solve tasks related to <b>algorithmics</b>, on sites such as
+            <b>CodeWars</b> <a class="noprint" target="_blank" href="https://www.codewars.com/users/Mikkael">[my profile]</a>
+            or <b>LeetCode</b>. <br>
             I also find <b>Maths</b> to be very fun.
         </p>
         <p>
@@ -37,7 +39,9 @@ const [lang, strings] = use_lang({
             <span class="soft"> (kompletnie niezwiązane ze sobą języki, ale tak wyszło)</span>. <br>
             Pierwszą styczność miałem z programowaniem w gimnazjum, i od tamtego czasu ciągle się rozwijam. <br>
             Traktuję programowanie jako bardzo rozbudowaną zagadkę logiczną <span class="soft">(a <b>uwielbiam zagadki logiczne</b>)</span>. <br>
-            Od czasu do czasu rozwiązuję zadania związane z <b>algorytmiką</b>, na takich stronach jak np. <b>CodeWars</b> czy <b>LeetCode</b>. <br>
+            Od czasu do czasu rozwiązuję zadania związane z <b>algorytmiką</b>, na takich stronach jak np.
+            <b>CodeWars</b> <a class="noprint" target="_blank" href="https://www.codewars.com/users/Mikkael">[mój profil]</a> 
+            czy <b>LeetCode</b>. <br>
             <b>Matematyka</b> mi w niczym nie straszna.
         </p>
         <p>
